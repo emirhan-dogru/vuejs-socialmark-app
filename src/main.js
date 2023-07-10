8a7c4549b840d15aef093e83a5e36979ad2b18a6
Emirhan Doğru <emirhan-dogru@hotmail.com>
2023-07-10 13:58:41 +0300

    yeni eklemeler
    json-server projeye eklendi
    axios projeye eklendi
    axios için global değişken tanımlandı ve baseurl belirtildi
    kayıt ol sistemi yapıldı

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from "./router";
 import "@/assets/style.css";
 import appHeader from "@/components/includes/appHeader";
 import appBookmarkList from "@/components/includes/appBookmarkList/BookmarkList";
+import { appAxios } from "@/utils/appAxios";
+
 const app = createApp(App);
 
 app.use(router);
 app.component("appHeader", appHeader);
 app.component("appBookmarkList", appBookmarkList);
+app.config.globalProperties.$appAxios = appAxios;
 app.mount("#app");
--- src/views/Register_view.vue
+++ src/views/Register_view.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="login_register_container">
     <h3 class="text-2xl text-center mb-3">Kayıt Ol</h3>
-    <input type="text" placeholder="Tam Ad" class="input mb-3" />
-    <input type="text" placeholder="Kullanıcı Adı" class="input mb-3" />
-    <input type="password" placeholder="Şifre" class="input mb-3" />
-    <button class="default-button">Kayıt ol</button>
+    <input v-model="userData.fullname" type="text" placeholder="Tam Ad" class="input mb-3" />
+    <input v-model="userData.username" type="text" placeholder="Kullanıcı Adı" class="input mb-3" />
+    <input v-model="userData.password" type="password" placeholder="Şifre" class="input mb-3" />
+    <button class="default-button" @click="onSave">Kayıt ol</button>
     <span class="text-center mt-3 text-sm">
       Zaten Üyeyim,
-      <a href="#" class="text-red-900 hover:text-black">Giriş yap!</a>
+      <router-link :to="{ name: 'LoginPage' }" class="text-red-900 hover:text-black"> Giriş yap</router-link>
     </span>
   </div>
 </template>
+
+<script>
+import cryptoJs from "crypto-js";
+export default {
+  data() {
+    return {
+      userData: {
+        username: null,
+        fullname: null,
+        password: null,
+      },
+    };
+  },
+  methods: {
+    onSave() {
+      const password = this.userData.password;
+      const key = "bookmark123!123";
+      const cryptedPassword = cryptoJs.AES.encrypt(password, key).toString();
+      this.$appAxios.post("/users", { ...this.userData, password: cryptedPassword }).then((res) => {
+        if (res.statusText === "Created") {
+          this.$router.push({ name: "homePage" });
+        }
+      });
+      // console.log(cryptedPassword);
+      //console.log(this.userData);
+    },
+  },
+};
+</script>
